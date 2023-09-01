bf81c5002d93672d3015b2b010d8a6fac739eb32
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2023-09-01 20:40:46 -0300

    taller 1.9

--- Taller_1_9/codigo/script.js
+++ Taller_1_9/codigo/script.js
@@ -65,7 +65,7 @@ contenedor.appendChild(boton)
 
 //Para que el <a> no quede al lado del Botón...
 
-a.style.display="block"
+//a.style.display="block"
 
 
 /*
@@ -73,3 +73,75 @@ a.style.display="block"
 6. Haciendo uso del DOM y eventos, lograr obtener los dos numeros que el usuario ingreso en los inputs de la zona “números” y devolver dentro del recuadro violeta el promedio de ambos.
 
 */
+
+//promedio : divido en dos el resultado de la suma
+
+//obtener el valor de los dos inputs y sacar el promedio
+/*
+let numero1=document.querySelector("#num1");
+let numero2=document.querySelector("#num2");
+*/
+const funcionEj6=(event)=>{
+  event.preventDefault()//para que no mande el formulario y reinicie (accion por defecto)
+
+
+let numero1=document.querySelector("#num1").value;
+let numero2=document.querySelector("#num2").value;
+/* numero1.value , numero2.value */
+
+    
+    let suma=parseInt(numero1)+parseInt(numero2);
+
+   
+    let promedio=suma/2;
+
+    //ponerlo en el recuadro violeta
+    let recuadro_violeta=document.querySelector(".violeta");
+    recuadro_violeta.innerHTML=`<p>${promedio}</p>`
+}
+
+/*
+
+7. Utilizando los métodos y propiedades vistas en DOM y un poco de eventos, lograr que al pasar el mouse por la imagen de la puerta ,esta se reemplace por la imagen del payaso.
+
+*/
+//onmouseover()
+//tomar a la imagen y cambiar su atributo src 
+let payaso=false;
+
+const cambioImagen=(nodoImg)=>{
+    console.dir(nodoImg)
+//nodoImg lo pasamos en HTML con this
+/*
+si queremos que cuando deje de pasar el mouse por encima vuelva a colocar la puerta */
+    if(payaso == false){
+         nodoImg.setAttribute("src","./images/payaso.jpg")
+         payaso=true
+    }else{
+        nodoImg.setAttribute("src","./images/puerta.jpg")
+        payaso=false
+    }
+   
+
+
+}
+
+/*
+si queremos que cuando deje de pasar el mouse por encima vuelva a colocar la puerta
+
+en la imagen (HTML)->onmouseleave="ponerPuerta()"
+
+const ponerPuerta=()=>{
+    let imagen=document.querySelector("#imagen-cambio");
+
+    imagen.setAttribute("src","./images/puerta.jpg")
+}
+*?
+
+//otra alternativa (sin el evento en el HTML);
+
+//dos maneras de utilizar eventos
+/* 
+imagen.addEventListener('mouseover',()=>{
+    imagen.setAttribute("src","./images/payaso.jpg")
+})*/
