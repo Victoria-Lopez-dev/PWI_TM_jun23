54655e7fa9552ffbb373c2f9e9a36f3714d0ec01
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2023-09-01 19:51:12 -0300

    taller 1.9

--- Asincronia/script.js
+++ Asincronia/script.js
@@ -1,2 +1,114 @@
+//eventos
+
+const mostrarNodo=(nodo)=>{
+    console.dir(nodo) //obtenemos directamente el nodo en el que nos encontramos
+}
+
+const mostrarNodo2=(evento)=>{
+    console.dir(evento.target)//obtenemos informacion del evento y dentro de esta informacion accedemos al elemento(nodo) en el que ocurrio el evento
+}
+
+//-------------------------------
+
+
+//Asincrona 
+
+//callbacks
+const saludo =()=>{
+    console.log("hola")
+}
+/* addEventListener*/
+let boton=document.querySelector("#boton-this");
+
+boton.addEventListener("click",saludo)
+
+const myDisplayer=(info)=>{
+    let texto=document.querySelector("p");
+    texto.textContent=info
+    return info
+}
+const  myCalculator=(num1,num2)=>{
+    let sum= num1+num2;
+    return sum
+    //myDisplayer(sum)
+}
+
+const  myCalculator2=(num1,num2,unaFuncion)=>{
+    let sum= num1+num2;
+    
+    unaFuncion(sum)
+}
+
+//usando asincronia -> con callback
+myCalculator2(2,2,myDisplayer)
+
+let numero=5;
+//funcion sincronica -> todo en simultaneo
+myDisplayer(numero)
+/*
+const  myCalculator2=(num1,num2,myDisplayer)=>{
+    let sum= num1+num2;
+    
+    myDisplayer(sum) ->ejecucion de una funcion 
+}
+
+*/
+
+//--------------------------
+//promesas
+const buscar =async()=>{
+
+    return await fetch("https://rickandmortyapi.com/api/character")
+    .then(data =>data.json()) 
+    .catch((err)=> err) 
+   
+}
+/*
+const buscarPersonajes=async ()=>{
+
+    let info= await buscar()
+       console.log(info)
+   }
+*/
+//callback (evento onclick)
+
+//promesa (busqueda de informacion = fetch)
+
+//async await -> espere a recibir la informacion para hacer cosas con ella
+
+//espera a que regrese la informacion que busco para procesarla (fetch/then/catch)
+//espera que termine de procesar la informacion para guardar en una variable y trabajarla (asyn/await)
+
+//espera a que se termine de hacer click en el boton para ejecutar todo lo anterior(evento click -> que es un callback)
+const buscarPersonajes=async ()=>{
+
+ let info= await fetch("https://rickandmortyapi.com/api/character")
+    .then(data =>data.json()) 
+    .catch((err)=> err) 
+    console.log(info)
+
+    //cosas que hacemos con la info recibida....
+
+    let personajes=info.results;//me trajo un array
+
+    for(let i=0;i<personajes.length;i++){
+        let imagen=document.createElement('img');
+
+    imagen.setAttribute("src",personajes[i].image)
+
+    let body= document.querySelector("body")
+    body.appendChild(imagen)
+    }
+    
+
+    
+}
+
+
+
+//buscarPersonajes()
+
+//async await
+//async va al inicio del a funcion y el await va delante de lo que queremos esperar
 
 
--- Taller_1_9/codigo/script.js
+++ Taller_1_9/codigo/script.js
@@ -1,1 +1,75 @@
 //documento vacio para que puedan completar el codigo de los ejercicios 
+
+/*
+ 1 -Utilizando lo visto de DOM ,cambiar el color de la tipografía de todos los <h2> a “orange”.
+*/
+
+let titulosh2=document.querySelectorAll("h2");//array
+
+
+const cambio_color=()=>{
+    for(let i=0; i < titulosh2.length; i++){
+        titulosh2[i].style.color="orange"
+   }
+}
+
+cambio_color()
+
+/*
+
+2. Lograr cambiar el tamaño de fuente del segundo párrafo del HTML a 16px.
+
+
+let segundo_parrafo=document.getElementById("#texto2");
+
+//segundo_parrafo.style.fontSize="16px";
+
+//alternativa
+segundo_parrafo.classList.add("texto16");
+
+let parrafos=document.querySelectorAll("p");//array con todos los <p>, y el segundo en la lista va a tener indice 1 por qeu arranca desde 0
+
+parrafos[1].style.fontSize="16px";
+*/
+
+/*
+
+3. Utilizando el DOM, lograr agregar el atributo alt con una pequeña descripción de la imagen que se encuentra debajo del titulo “Cambiando la imagen”.
+
+*/
+//setAtributte()
+
+let imagen=document.querySelector("#imagen-cambio");
+
+imagen.setAttribute("alt","una puerta");
+
+
+/*
+4. Crear un boton con el valor “Pulsar” y agregarlo luego del titulo “Acciones” - Todo desde el DOM
+*/
+
+let boton=document.createElement("button")
+boton.innerText="Pulsar";
+
+/*
+una alternativa 
+let section=document.querySelector(".un_section")
+let a=document.querySelector("a") 
+section.insertBefore(boton, a)*/
+//otra alternativa
+
+let contenedor=document.querySelector(".contenedor-boton")
+contenedor.appendChild(boton)
+
+
+
+//Para que el <a> no quede al lado del Botón...
+
+a.style.display="block"
+
+
+/*
+
+6. Haciendo uso del DOM y eventos, lograr obtener los dos numeros que el usuario ingreso en los inputs de la zona “números” y devolver dentro del recuadro violeta el promedio de ambos.
+
+*/
