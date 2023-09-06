d9437e3470837c48a4ae476b57eec835cd222ba8
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2023-09-06 11:02:42 -0300

    clase 20

---
+++ Clase 19-20/React/primer-proyecto/src/componentes/Navegacion/navegacion.jsx
@@ -0,0 +1,15 @@
+import './navegacion.css'
+
+function Navegacion() {
+    return(
+        <nav>
+            <ul className="menu">
+                <li>item menu</li>
+                <li>item menu</li>
+            </ul>
+            
+        </nav> 
+    )
+}
+
+export default Navegacion
