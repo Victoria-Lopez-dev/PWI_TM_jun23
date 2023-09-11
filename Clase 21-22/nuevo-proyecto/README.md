756e5e23d0b5b8f833a553e2d4178c1042c3815c
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2023-09-11 07:36:03 -0300

    clase 21

---
+++ Clase 21-22/nuevo-proyecto/README.md
@@ -0,0 +1,6 @@
+# Segundo proyecto con React
+
+En este proyecto vamos a ver lo que son los Hooks, como poder utilizarlos,que son los estados y como aplicar eventos para modificar los mismos
+
+Tambien utilizaremos este mismo proyecto para armar listas e iteraciones junto con el uso de una nueva herramienta propia de React: Fragment.
+Junto con esto, realizaremos pedidos a una API y como reproducirlo en nuestro sitio 
--- proyecto_React/proyecto/src/pages/Home/Home.js
+++ proyecto_React/proyecto/src/pages/Home/Home.js
@@ -1,6 +1,8 @@
-function Home() {
+import './Home.css';
+
+export default function Home() {
     return(
-        <div>
+        <div className="bg-primary w-75 p-5">
             <h1>Proyect Rick & Morty</h1>
             <h2>Helcome to Rick & Morty Proyect!</h2>
             <p>This proyect was made for practising React and to made a functional website
@@ -8,8 +10,8 @@ function Home() {
             In this website you can know information of the characters of this animated series.
             Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
             <h3>Lets go!</h3>
-            <button>Charactes</button>
-            <button>Contact</button>
+            <button className='btn btn-danger p-2 me-2'>Charactes</button>
+            <button className='btn btn-danger p-2 me-2'>Contact</button>
         </div>
     )
 }
