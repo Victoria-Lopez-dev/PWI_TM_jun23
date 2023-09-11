e82df24b976569f47b7276036281cd0c375a9e10
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2023-09-11 08:41:39 -0300

    clase 21

--- Clase 21-22/nuevo-proyecto/src/App.js
+++ Clase 21-22/nuevo-proyecto/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    
     </div>
   );
 }
 
+
+
+
+
+
+
+
+//------ si lo hacemos con componentes de clase
+
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       nombre:"Ana"
+//     }
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <h1>{this.state.nombre}</h1>
+//         <button onClick={()=>this.setState({nombre:"Juan"})}>Click</button>
+//       </div>
+//       )
+//   }
+// }
 export default App;
